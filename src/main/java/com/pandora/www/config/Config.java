2735b931041dc9e63c02aa5f09012c49299c6918
CPZ <cpz77585211@163.com>
2024-09-26 16:44:54 +0800

    init code.

---
+++ src/main/java/com/pandora/www/config/Config.java
@@ -0,0 +1,25 @@
+package com.pandora.www.config;
+
+import org.slf4j.Logger;
+
+import java.util.Properties;
+
+public class Config {
+    public static String env;
+    public static String dataUrl;
+    public static String kerberosUsername;
+    public static String user;
+    public static String password;
+    public static String cjStartDay;
+    public static String cjEndDay;
+    public static String bjStartDay;
+    public static String bjEndDay;
+    public static String linesOfOneFile;
+
+//    private Logger
+
+    static {
+        Properties properties = new Properties();
+
+    }
+}
